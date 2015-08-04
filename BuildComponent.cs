6f26624fd7b4cbc6424f4c7c9b01d3358979c67d
myrovh <myrovh@yandex.com>
2015-08-04 15:50:32 +1000

    added script component with custom gui

---
+++ BuildComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class BuildComponent : MonoBehaviour
+{
+
+    public string BuildModuleName;
+    public bool RuntimeRebuild;
+    public float BuildInterval;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Editor/BuildComponentEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BuildComponent))]
+class BuildComponentEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Build Now"))
+            Debug.Log("It's alive: " + target.name);
+    }
+}
